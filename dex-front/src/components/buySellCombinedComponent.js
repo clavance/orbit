bb3468200bd70e496b51ab7cc116696daa7582aa
Darius <apple@MacBook-Pro.home>
2019-02-27 00:14:16 +0000

    moved all files

--- dex-front/src/components/buySellCombinedComponent.js
+++
@@ -1,5 +0,0 @@
-import React, { Component } from 'react';
-import ReactTable from "react-table";
-import "react-table/react-table.css";
-import "./component.css"
-import Fade from 'react-reveal/Fade';
